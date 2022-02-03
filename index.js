285b66dad29e700a0ea93d1121a504db560fefa6
josueJURE <josue.jure@gmail.com>
2022-02-03 18:10:39 +0000

    use onkeyup event on input element

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 
 const inputBox = document.querySelector(".inputField input");
 const addBtn = document.querySelector(".inputField button");
+
+inputBox.onkeyup = () => {
+  let userData = inputBox.value;
+  if(userData.trim()! = 0) {
+  }
+}
