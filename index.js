2fefc9047876813f5290479d1de6baefd2ec2f16
josueJURE <josue.jure@gmail.com>
2023-04-05 17:34:25 +0100

    add margin-top css rule to element with class addTakSection

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const userInput = document.getElementById("userInput");
 const button = document.getElementById("button");
 const taskList = document.getElementById("taskList");
 
-button.addEventListener('click', addTaskToList);
+button.addEventListener("click", addTaskToList);
 
 function addTaskToList() {
-    const element = document.createElement("div");
-    element.textContent = "";
-    element.textContent = userInput.value;
-    taskList.appendChild(element);
-  
+  const element = document.createElement("div");
+  element.textContent = "";
+  element.textContent = userInput.value;
+  taskList.appendChild(element);
 }
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === 13) {
+    addTaskToList();
+  }
+});
+
+// userInput.addEventListener('keydown', function(event) {
+//     if (event.keyCode === 13) {
+//         addTaskToList();
+//     }
+// });
