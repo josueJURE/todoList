e01dc87042af6c8414fd8e264cd89ffe66de5ecf
josueJURE <josue.jure@gmail.com>
2023-04-05 17:44:40 +0100

    modify addTaskToList() so that input element clears after user's clicked on button

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ button.addEventListener("click", addTaskToList);
 
 function addTaskToList() {
   const element = document.createElement("div");
-  element.textContent = "";
   element.textContent = userInput.value;
   taskList.appendChild(element);
+  userInput.value = "";
 }
 
-document.addEventListener("keydown", function (e) {
-  if (e.key === 13) {
-    addTaskToList();
-  }
-});
+// document.addEventListener("keydown", function (e) {
+//   if (e.key === 13) {
+//     addTaskToList();
+//   }
+// });
 
 // userInput.addEventListener('keydown', function(event) {
 //     if (event.keyCode === 13) {
