cbad42462543096a40bb3eeebf78d886ff9a53d3
josueJURE <josue.jure@gmail.com>
2023-04-05 17:18:24 +0100

    put addEventListener on button

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const userInput = document.getElementById("userInput");
-console.log(userInput)
+const button = document.getElementById("button");
+const taskList = document.getElementById("taskList");
+
+button.addEventListener('click', addTaskToList);
+
+function addTaskToList() {
+    const element = document.createElement("div");
+    element.textContent = userInput.value;
+    taskList.appendChild(element);
+    element.textContent = " ";
+}
