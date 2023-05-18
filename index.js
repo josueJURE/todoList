5cd4a5cc60445419dd371e52845e870785b3f88a
josueJURE <josue.jure@gmail.com>
2023-05-18 17:25:05 +0100

    create howManyTasksUserHas() and use local storage array to update it

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const numberOfTasks = document.querySelector(".numberOfTasks")
 
 window.addEventListener("load", loadTasksFromLocalStorage);
 add.addEventListener("click", addTaskToList);
+
+
 taskList.addEventListener("click", crossTaskOut);
 taskList.addEventListener("click", deleteTask);
 arrowUp.addEventListener("click", scrollAllTheWayUp);
@@ -65,8 +67,8 @@ function addTaskToList() {
   </div>`;
   userInput.value = "";
 
-  const icons = document.querySelectorAll(".fa-trash");
-  icons.forEach((icon) => icon.addEventListener("click", deleteTask));
+  // const icons = document.querySelectorAll(".fa-trash");
+  // icons.forEach((icon) => icon.addEventListener("click", deleteTask));
 
   if (DEBUG) {
     console.log("addTaskToList:", "taskList.children:", taskList.children);
@@ -133,32 +135,38 @@ function loadTasksFromLocalStorage() {
        <i class="fas fa-edit"></i>
     </div>`;
 
+
+    numberOfTasks.innerHTML = howManyTasksUserHas(tasks.length)
+
     const draggableElementsArray = document.querySelectorAll(".dropzone");
     console.log(draggableElementsArray);
 
-    taskList.addEventListener("click", editContent);
 
-    function editContent(e) {
-      const element = e.target;
-      if (element.classList.contains("fa-edit")) {
-        const prevSibling =
-          element.previousElementSibling.previousElementSibling;
-
-        prevSibling.parentElement.addEventListener("mouseover", function (e) {
-          const index = tasks.findIndex(
-            (task) => task.id === Number(e.srcElement.dataset.id)
-          );
-          if (index !== -1) {
-            prevSibling.contentEditable = true;
-            const tasks = getTasksFromLocalStorage();
-            tasks[index].text = prevSibling.textContent;
-            localStorage.setItem("tasks", JSON.stringify(tasks));
-          }
-        });
-        prevSibling.contentEditable = false;
-      }
-    }
   });
+
+  taskList.addEventListener("click", editContent);
+
+  function editContent(e) {
+    console.log("clicked")
+    const element = e.target;
+    if (element.classList.contains("fa-edit")) {
+      const prevSibling =
+        element.previousElementSibling.previousElementSibling;
+
+      prevSibling.parentElement.addEventListener("mouseover", function (e) {
+        const index = tasks.findIndex(
+          (task) => task.id === Number(e.srcElement.dataset.id)
+        );
+        if (index !== -1) {
+          prevSibling.contentEditable = true;
+          const tasks = getTasksFromLocalStorage();
+          tasks[index].text = prevSibling.textContent;
+          localStorage.setItem("tasks", JSON.stringify(tasks));
+        }
+      });
+      prevSibling.contentEditable = false;
+    }
+  }
 
   if (DEBUG) {
     console.log("loadTasksFromLocalStorage:", "# of tasks:", tasks.length);
@@ -174,6 +182,8 @@ function addTaskToLocalStorage(task) {
 
   const tasks = getTasksFromLocalStorage();
   tasks.push(task);
+  numberOfTasks.innerHTML = howManyTasksUserHas(tasks.length)
+ 
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
@@ -196,6 +206,7 @@ function deleteTaskFromLocalStorage(id) {
   const tasks = getTasksFromLocalStorage();
   const index = tasks.findIndex((task) => task.id === Number(id));
   tasks.splice(index, 1);
+  numberOfTasks.innerHTML = howManyTasksUserHas(tasks.length)
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
@@ -294,6 +305,14 @@ function scrollAllTheWayDown() {
     top: taskList.scrollHeight,
     behavior: "smooth",
   });
+}
+
+function howManyTasksUserHas(arrayLength) {
+  if(arrayLength < 2) {
+    return `you have ${arrayLength} task left`
+  } else {
+    return `you have ${arrayLength} tasks left`
+  }
 }
 
 checkForDuplicates();
